22eef1898c6e8942668192fbe38b10ba1ba4ffac
acbserrano <121968671+acbserrano@users.noreply.github.com>
2023-05-18 23:40:41 +0100

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,8 +3,6 @@ import React from "react";
 import "./SearchEngine.css";
 
 export default function Meaning(props) {
-  console.log(props.meaning.synonyms);
-
   return (
     <div className="meaning">
       <div className="partOfSpeech">{props.meaning.partOfSpeech}</div>
